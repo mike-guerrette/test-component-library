e36b6d653c952c385dd7d82443c3e082bba80062
Mike Guerrette <mike@robinpowered.com>
2018-11-30 10:26:56 -0500

    Add Colors to core exports

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,8 +1,8 @@
 import styled from 'react-emotion';
-import {Spacing} from '@robinpowered/ui-core';
+import {Spacing, Colors} from '@robinpowered/ui-core';
 
 const Card = styled.div({
-  border: '1px solid gray',
+  border: `1px solid ${Colors.UI.Border.Default}`,
   padding: Spacing.Small,
   color: 'blue',
   backgroundColor: 'white'
--- src/core/index.js
+++ src/core/index.js
@@ -1,8 +1,9 @@
 import Spacing from './styles/spacing';
-
+import Colors from './styles/colors';
 import Button from './components/Button';
 
 export {
   Spacing,
+  Colors,
   Button
 };
