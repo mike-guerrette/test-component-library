549ed9a18872af15699a358e52e5dbdc8a2f5a67
Mike Guerrette <mike@robinpowered.com>
2018-11-30 11:32:45 -0500

    Update spacing

--- src/Containers/dist/index.js
+++ src/Containers/dist/index.js
@@ -8,19 +8,13 @@ var _reactEmotion = require('react-emotion');
 
 var _reactEmotion2 = _interopRequireDefault(_reactEmotion);
 
-var _uiCore = require('@robinpowered/ui-core');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(_uiCore.Colors.UI.Background.Blue);
-
-var Card = /*#__PURE__*/(0, _reactEmotion2.default)('div', {
+var BaseContainer = /*#__PURE__*/(0, _reactEmotion2.default)('div', {
   target: 'e1r1p2t30'
 })({
-  border: '1px solid gray',
-  padding: _uiCore.Spacing.Small,
-  color: 'blue',
-  backgroundColor: _uiCore.Colors.UI.Background.White
+  padding: 20,
+  display: 'flex'
 });
 
-exports.default = Card;
+exports.default = BaseContainer;
---
+++ src/core/dist/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Button = exports.Colors = exports.Spacing = undefined;
+
+var _spacing = require('./styles/spacing');
+
+var _spacing2 = _interopRequireDefault(_spacing);
+
+var _colors = require('./styles/colors');
+
+var _colors2 = _interopRequireDefault(_colors);
+
+var _Button = require('./components/Button');
+
+var _Button2 = _interopRequireDefault(_Button);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Spacing = _spacing2.default;
+exports.Colors = _colors2.default;
+exports.Button = _Button2.default;
